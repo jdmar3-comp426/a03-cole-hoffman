9de67855e918cb651743d3632930eb268cda05fb
cole-hoffman <cwh@live.unc.edu>
2021-11-26 18:01:35 -0700

    currryyying

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -61,7 +61,7 @@ export const multiplyBy = (num1) => {
  * Use the multiplyBy function to create and export a function named
  *   "tenTimes" that multiplies a number by 10.
  */
-export const tenTimes = (num1) => {multiplyBy(num1)(10)};
+export const tenTimes = (num1) => {multiplyBy(10)(num1)};
 
 
 /**
